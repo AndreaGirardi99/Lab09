f9b6fc428b104b4504c6f76ee45c6c64fd3a5058
ADMIN <ADMIN@172.22.18.13>
2022-05-22 11:32:57 +0200

    OK

--- src/main/java/it/polito/tdp/borders/FXMLController.java
+++ src/main/java/it/polito/tdp/borders/FXMLController.java
@@ -1,44 +1,73 @@
-
 package it.polito.tdp.borders;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.borders.model.Country;
 import it.polito.tdp.borders.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
 
 	private Model model;
-	
-    @FXML // ResourceBundle that was given to the FXMLLoader
-    private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
-    private URL location;
+	@FXML // ResourceBundle that was given to the FXMLLoader
+	private ResourceBundle resources;
 
-    @FXML // fx:id="txtAnno"
-    private TextField txtAnno; // Value injected by FXMLLoader
+	@FXML // URL location of the FXML file that was given to the FXMLLoader
+	private URL location;
 
-    @FXML // fx:id="txtResult"
-    private TextArea txtResult; // Value injected by FXMLLoader
+	@FXML // fx:id="txtAnno"
+	private TextField txtAnno; // Value injected by FXMLLoader
 
-    @FXML
-    void doCalcolaConfini(ActionEvent event) {
+	@FXML // fx:id="txtResult"
+	private TextArea txtResult; // Value injected by FXMLLoader
 
-    }
+	@FXML
+	private ComboBox<Country> cmbNazione;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        assert txtAnno != null : "fx:id=\"txtAnno\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+	@FXML
+	void doCalcolaConfini(ActionEvent event) {
+		txtResult.clear();
+		int anno;
+		try {
+			anno = Integer.parseInt(txtAnno.getText());
+			if ((anno < 1816) || (anno > 2016)) {
+				txtResult.setText("Inserire un anno nell'intervallo 1816 - 2016");
+				return;
+			}
+		} catch (NumberFormatException e) {
+			txtResult.setText("Inserire un anno nell'intervallo 1816 - 2016");
+			return;
+		}
+		model.creaGrafo(anno);
+		List<Country> countries = model.getCountries();
+		cmbNazione.getItems().addAll(countries);
+		txtResult.appendText(String.format("Numero componenti connesse: %d\n", model.getNumberOfConnectedComponents()));
+		txtResult.appendText(model.getV());
+	}
 
-    }
-    
-    public void setModel(Model model) {
-    	this.model = model;
-    }
+	@FXML
+	void doTrovaRaggiungibili(ActionEvent event) {
+		txtResult.clear();
+		Country start = cmbNazione.getSelectionModel().getSelectedItem();
+		List<Country> l = model.doIteratore(start);
+		txtResult.appendText(l.toString());
+	}
+
+	@FXML // This method is called by the FXMLLoader when initialization is complete
+	void initialize() {
+		assert txtAnno != null : "fx:id=\"txtAnno\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert cmbNazione != null : "fx:id=\"cmbNazione\" was not injected: check your FXML file 'Borders.fxml'.";
+	}
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
 }
--- src/main/java/it/polito/tdp/borders/model/Model.java
+++ src/main/java/it/polito/tdp/borders/model/Model.java
@@ -1,11 +1,99 @@
 package it.polito.tdp.borders.model;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import org.jgrapht.Graphs;
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.traverse.DepthFirstIterator;
+
+import it.polito.tdp.borders.db.BordersDAO;
+
 public class Model {
+	BordersDAO dao = new BordersDAO();
+	private SimpleGraph<Country, DefaultEdge> graph;
+	List<Country> countries;
 
 	public Model() {
-		
-		
-		
 	}
 
+	public void creaGrafo(int anno) {
+		graph = new SimpleGraph<Country, DefaultEdge>(DefaultEdge.class);
+		// countries = dao.getCountryByYears(anno);
+
+		for (Border b : dao.getCountryPairs(anno)) {
+			graph.addVertex(b.getC1());
+			graph.addVertex(b.getC2());
+			graph.addEdge(b.getC1(), b.getC2());
+			aggiornaDegree();
+		}
+		countries = new ArrayList<>(graph.vertexSet());
+
+		System.out.println("ARCHI: ");
+		System.out.println(graph.edgeSet());
+		System.out.println("\nVERTICI e grado: ");
+		System.out.println(this.getV());
+		System.out.println("NUM VERTICI:" + graph.vertexSet().size());
+		System.out.println("NUM ARCHI:" + graph.edgeSet().size());
+		System.out.println("NUM COMP CONNESSE:" + this.getNumberOfConnectedComponents());
+	}
+
+	public String getV() {
+		String s = "";
+		for (Country c : graph.vertexSet()) {
+			s += c.getNomeDegree();
+		}
+		return s;
+	}
+
+	public void aggiornaDegree() {
+		for (Country c : graph.vertexSet()) {
+			c.setDegree(graph.degreeOf(c));
+		}
+	}
+
+	public int getNumberOfConnectedComponents() {
+		if (graph == null) {
+			throw new RuntimeException("Grafo non esistente");
+		}
+
+		ConnectivityInspector<Country, DefaultEdge> ci = new ConnectivityInspector<Country, DefaultEdge>(graph);
+		return ci.connectedSets().size();
+	}
+
+	public List<Country> getCountries() {
+		return countries;
+	}
+
+	public List<Country> doIteratore(Country start) {
+		List<Country> result = new ArrayList<Country>();
+		DepthFirstIterator<Country, DefaultEdge> it = new DepthFirstIterator<Country, DefaultEdge>(graph, start);
+		while (it.hasNext()) {
+			result.add(it.next());
+		}
+		return result;
+	}
+
+	/*
+	 * VERSIONE RICORSIVA
+	 */
+	private List<Country> doRicorsiva(Country start) {
+		List<Country> visited = new LinkedList<Country>();
+		visitaRicorsiva(start, visited);
+		return visited;
+	}
+
+	private void visitaRicorsiva(Country n, List<Country> visited) {
+		// Do always
+		visited.add(n);
+		// cycle
+		for (Country c : Graphs.neighborListOf(graph, n)) {
+			// filter
+			if (!visited.contains(c))
+				visitaRicorsiva(c, visited);
+			// DO NOT REMOVE!! (no backtrack)
+		}
+	}
 }
